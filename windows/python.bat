2555a3dce50958a12af1a0eb093010efa8376f9b
Jamie Gorson <jamie@gorsonfamily.com>
2018-04-03 12:22:56 -0500

    fixed quotes problem

--- windows/python.bat
+++ windows/python.bat
@@ -10,16 +10,16 @@ set /p RESULT=<tmpFile
 
 del tmpFile 
 
-type %1>tmpFile 
-set /p PYTHON_PROGRAM=<tmpFile 
-del tmpFile 
+type %1>tmpFile.txt 
+set /p PYTHON_PROGRAM=<tmpFile.txt 
+del tmpFile.txt
 
 
 set PYTHON_L=%* 
 
 REM display the compiler output to the user
+echo !RESULT!
 
-
-curl --request POST "http://mighty-lowlands-48531.herokuapp.com/polls/" --data-urlencode "student_id=%STUDENT_ID%" --data-urlencode "pyCall=%*" --data-urlencode "pyProgram=!PYTHON_PROGRAM!" --data-urlencode "pyOutput=!RESULT!"
+curl --request POST "http://mighty-lowlands-48531.herokuapp.com/polls/"  --data-urlencode "student_id=%STUDENT_ID%" --data-urlencode "pyCall=%*" --data-urlencode "pyProgram@%*" --data-urlencode "pyOutput=!RESULT!"
 
 )
