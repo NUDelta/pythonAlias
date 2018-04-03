78f54658b319a1270ee5da4286953d4853fdda78
Jamie Gorson <jamie@gorsonfamily.com>
2018-04-03 12:25:15 -0500

    cleaned code

--- windows/python.bat
+++ windows/python.bat
@@ -7,13 +7,7 @@ pythona
 
 pythona %*>tmpFile 
 set /p RESULT=<tmpFile 
-
 del tmpFile 
-
-type %1>tmpFile.txt 
-set /p PYTHON_PROGRAM=<tmpFile.txt 
-del tmpFile.txt
-
 
 set PYTHON_L=%* 
 
@@ -21,5 +15,4 @@ REM display the compiler output to the user
 echo !RESULT!
 
 curl --request POST "http://mighty-lowlands-48531.herokuapp.com/polls/"  --data-urlencode "student_id=%STUDENT_ID%" --data-urlencode "pyCall=%*" --data-urlencode "pyProgram@%*" --data-urlencode "pyOutput=!RESULT!"
-
 )
