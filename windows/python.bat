7872cc2594663fa8625d91206168e8e78e67ff34
Jamie Gorson <jamie@gorsonfamily.com>
2018-04-04 21:09:55 -0500

    fixed python bat to get both prints and errors

--- windows/python.bat
+++ windows/python.bat
@@ -5,14 +5,8 @@ IF  "%*"=="" (
 pythona 
 )ELSE (
 
-pythona %*>tmpFile 
-set /p RESULT=<tmpFile 
-del tmpFile 
-
-set PYTHON_L=%* 
-
-REM display the compiler output to the user
-echo !RESULT!
-
-curl --request POST "http://mighty-lowlands-48531.herokuapp.com/polls/"  --data-urlencode "student_id=%STUDENT_ID%" --data-urlencode "pyCall=%*" --data-urlencode "pyProgram@%*" --data-urlencode "pyOutput=!RESULT!"
+pythona %* > tmpFile.txt 2>&1 
+type tmpFile.txt
+curl --request POST "http://mighty-lowlands-48531.herokuapp.com/polls/"  --data-urlencode "student_id=%STUDENT_ID%" --data-urlencode "pyCall=%*" --data-urlencode "pyProgram@%*" --data-urlencode "pyOutput@tmpFile.txt" 
+del tmpFile.txt
 )
