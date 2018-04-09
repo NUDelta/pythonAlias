c9015e4f91da769b7917bad3d0f3e9b79f87c59f
Jamie Gorson <jamie@gorsonfamily.com>
2018-04-09 17:56:30 -0500

    fixed python 3 issue

--- mac/setup.sh
+++ mac/setup.sh
@@ -3,20 +3,23 @@
 if [[ $# -lt 1 ]]
 then
 	echo ""
-	echo " USAGE: ./setup.sh [StudentID]"
-	echo " ex: ./setup.sh 12345678"
+	echo " USAGE: ./setup.sh [StudyID]"
+	echo " ex: ./setup.sh 12345"
 	echo ""
 	exit
 fi
 
 #grab student ID
-ID=$1
+id=$1
 shift
 
 #Download the appropriate compiler
 	curl -k https://raw.githubusercontent.com/NUDelta/pythonAlias/master/mac/pythonseer.sh -o ~/pythonseer.sh
+	curl -k https://raw.githubusercontent.com/NUDelta/pythonAlias/master/mac/pythonseer3.sh -o ~/pythonseer3.sh
 	echo 'alias python="~/pythonseer.sh"' >> ~/.bash_profile
-	echo "export STUDENT_ID=\"$ID\"" >> ~/.bash_profile
+	echo 'alias python3="~/pythonseer3.sh"' >> ~/.bash_profile
+
+	echo "export STUDENT_ID=\"ID\"" >> ~/.bash_profile
 	echo "export PYTHONSEER_URL=\"http://mighty-lowlands-48531.herokuapp.com\"" >> ~/.bash_profile
 	echo "alias has been setup. please close and reopen the terminal."
 
